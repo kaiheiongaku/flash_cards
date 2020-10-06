1cf929ceb5e6a51bd19ffe731619a7b872962071
Robert Heath <robert.b.heathii@gmail.com>
2020-10-06 16:45:08 -0500

    Create test for Round initialization

---
+++ test/round_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/round'
+
+class RoundTest < Minitest::Test
+
+  def test_it_exists_and_has_attributes
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and
+      reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5°
+      clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    assert_instance_of Round, round
+    assert_equal deck, round.deck
+    assert_equal [], round.turns
+  end
+end
