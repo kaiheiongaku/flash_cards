bf2547db974e630dcbed0a63884bdf65ccb83d04
Robert Heath <robert.b.heathii@gmail.com>
2020-10-06 16:55:09 -0500

    Initialize round with deck and turns

---
+++ lib/round.rb
@@ -0,0 +1,12 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+
+class Round
+  attr_reader :deck, :turns
+
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+  end
+end
