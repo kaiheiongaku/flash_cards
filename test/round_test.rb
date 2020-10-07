07c0694e376a4f503c198bf266237509fe302832
Robert Heath <robert.b.heathii@gmail.com>
2020-10-07 12:26:24 -0500

    Write test for percent correct by category

--- test/round_test.rb
+++ test/round_test.rb
@@ -56,4 +56,11 @@ class RoundTest < Minitest::Test
     @round.take_turn("Venus")
     assert_equal 50.0, @round.percent_correct
   end
+
+  def test_percent_correct_by_category
+    new_turn = @round.take_turn("Juneau")
+    @round.take_turn("Venus")
+    assert_equal 100.0, @round.percent_correct_by_category(:Geography)
+    assert_equal 0.0, @round.percent_correct_by_category(:STEM)
+  end
 end
