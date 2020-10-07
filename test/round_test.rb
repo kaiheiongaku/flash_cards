2a93076e3cfd184b93835a3824367843dd196136
Robert Heath <robert.b.heathii@gmail.com>
2020-10-07 11:25:23 -0500

    Create test for number_correct_by_category

--- test/round_test.rb
+++ test/round_test.rb
@@ -42,4 +42,11 @@ class RoundTest < Minitest::Test
     @round.take_turn("Venus")
     assert_equal 1, @round.number_correct
   end
+
+  def test_number_correct_by_category
+    new_turn = @round.take_turn("Juneau")
+    @round.take_turn("Venus")
+    assert_equal 1, @round.number_correct_by_category(:Geography)
+    assert_equal 0, @round.number_correct_by_category(:STEM)
+  end
 end
