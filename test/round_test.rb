62b6b18324deeba8a09e9cde8ffc00c1110853db
Robert Heath <robert.b.heathii@gmail.com>
2020-10-06 20:05:11 -0500

    Update Deck and Round tests so that they utilize setup

--- test/round_test.rb
+++ test/round_test.rb
@@ -4,17 +4,19 @@ require './lib/round'
 
 class RoundTest < Minitest::Test
 
-  def test_it_exists_and_has_attributes
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and
       reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5°
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5°
       clockwise from due north?", "North north west", :STEM)
-    deck = Deck.new([card_1, card_2, card_3])
-    round = Round.new(deck)
+    @deck = Deck.new([@card_1, @card_2, @card_3])
+    @round = Round.new(@deck)
+  end
 
-    assert_instance_of Round, round
-    assert_equal deck, round.deck
-    assert_equal [], round.turns
+  def test_it_exists_and_has_attributes
+    assert_instance_of Round, @round
+    assert_equal @deck, @round.deck
+    assert_equal [], @round.turns
   end
 end
